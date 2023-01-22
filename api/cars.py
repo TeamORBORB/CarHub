88abbba33497e4fd91d1a0c570af3355c468f0f3
muddydog25 <etran8@gmail.com>
2023-01-22 02:01:35 -0800

    Revert "api :D"
    This reverts commit dde4f3d69600cae18f1d51fdc4c3ca53e7edcfa0.

--- api/cars.py
+++ api/cars.py
@@ -1,32 +1,32 @@
-from flask import Blueprint, request, jsonify
-import requests
-from flask_restful import Api, Resource
+# from flask import Blueprint, request, jsonify
+# import requests
+# from flask_restful import Api, Resource
 
-cars_api = Blueprint('cars_api', __name__ ,
-                    url_prefix='/api/cars')
+# cars_api = Blueprint('cars_api', __name__ ,
+#                     url_prefix='/api/cars')
 
-api = Api(cars_api)
+# api = Api(cars_api)
 
-def carsAPI():
-    global cars_api  
-    try: cars_info
-    except: cars_info = None
+# def carsAPI():
+#     global cars_api  
+#     try: cars_info
+#     except: cars_info = None
        
-    url = "https://car-data.p.rapidapi.com/cars"
+#     url = "https://car-data.p.rapidapi.com/cars"
 
-    querystring = {"limit":"49","page":"0"}
+#     querystring = {"limit":"49","page":"0"}
 
-    headers = {
-	"X-RapidAPI-Key": "d3a3e94748msh74bb629320d5734p160ceajsn7f28f4859ea2",
-	"X-RapidAPI-Host": "car-data.p.rapidapi.com"
-    }
-    response = requests.request("GET", url, headers=headers, params=querystring)
-    cars_info = response
-    return response
+#     headers = {
+# 	"X-RapidAPI-Key": "d3a3e94748msh74bb629320d5734p160ceajsn7f28f4859ea2",
+# 	"X-RapidAPI-Host": "car-data.p.rapidapi.com"
+#     }
+#     response = requests.request("GET", url, headers=headers, params=querystring)
+#     cars_info = response
+#     return response
 
-class CarsAPI(Resource):
-    def get(self):
-        response = carsAPI()
-        return response.json()
+# class CarsAPI(Resource):
+#     def get(self):
+#         response = carsAPI()
+#         return response.json()
 
-api.add_resource(CarsAPI, '/')
+# api.add_resource(CarsAPI, '/')
