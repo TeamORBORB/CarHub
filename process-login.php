c5c65f8553d46dc3d1155f5dc881799ce1bc300c
20730661 <111465192+hsinaDitaM@users.noreply.github.com>
2023-02-13 16:28:44 -0800

    dealership stuff

--- process-login.php
+++ process-login.php
@@ -4,4 +4,17 @@ $email = $_POST['email'];
 $uname = $_POST['uname'];
 $psw = filter_input(INPUT_POST, 'psw', FILTER_VALIDATE_INT);
 
-var_dump($email, $uname , $psw);
+$host = "localhost";
+$dbname= "users_db";
+$username= "root";
+$password= "";
+
+$conn=mysqli_connect(hostname:$host, username:$username, password:$password, database:$dbname); 
+
+
+if (mysqli_connect_errno()) {
+    die("Failed to connect to MySQL: " . mysqli_connect_error());
+}
+
+echo "Connected to MySQL";
+
